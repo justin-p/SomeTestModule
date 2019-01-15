8e006a6e719c004a9052ad67699796e777e0c95c
justin-p <justin-p@users.noreply.github.com>
2019-01-15 11:58:53 +0100

    webhook test

--- src/public/Write-SomeTestModule.ps1
+++ src/public/Write-SomeTestModule.ps1
@@ -9,22 +9,21 @@ Function Write-SomeTestModule {
     .EXAMPLE
         Write-SomeTestModule
     #>
-        [CmdletBinding()]
-        param(
-            $a
-        )
-        begin {
-            if ($script:ThisModuleLoaded -eq $true) {
-                Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
-            }
-            $FunctionName = $MyInvocation.MyCommand.Name
-            Write-Verbose "$($FunctionName): Begin."
+    [CmdletBinding()]
+    param(
+        $a
+    )
+    begin {
+        if ($script:ThisModuleLoaded -eq $true) {
+            Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
         }
-        process {
-            Write-Output 'Yerp. This is a function.'
-        }
-        end {
-            Write-Verbose "$($FunctionName): End."
-        }
-
+        $FunctionName = $MyInvocation.MyCommand.Name
+        Write-Verbose "$($FunctionName): Begin."
     }
+    process {
+        Write-Output 'Yerp. This is a function.'
+    }
+    end {
+        Write-Verbose "$($FunctionName): End."
+    }
+}
